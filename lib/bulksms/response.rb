a7cfc45c7487ae8202ec273a09135f91677d1dab
Julien Gauby <julien.gauby@gmail.com>
2012-03-07 10:08:16 +0100

    adding raw response of API to object response

--- lib/bulksms/response.rb
+++ lib/bulksms/response.rb
@@ -1,12 +1,13 @@
 module Bulksms
   class Response
 
-    attr_reader :code, :result, :batch_id
+    attr_reader :raw_response, :code, :result, :batch_id
 
-    def initialize(code, result, batch_id)
-      @code      = code
-      @result    = result
-      @batch_id  = batch_id
+    def initialize(raw_response, code, result, batch_id)
+      @raw_response = raw_response
+      @code         = code
+      @result       = result
+      @batch_id     = batch_id
     end
 
     def success?
@@ -15,7 +16,7 @@ module Bulksms
 
     def self.parse(response_text)
       tokens = response_text.split('|')
-      new tokens[0].to_i, tokens[1], tokens[2].to_i
+      new response_text, tokens[0].to_i, tokens[1], tokens[2].to_i
     end
 
   end
